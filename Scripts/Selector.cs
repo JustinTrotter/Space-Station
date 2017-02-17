faaa9005b705e9b5b227140ae3288aaab7774e90
Lambinvoker <trotter.justin@gmail.com>
2017-02-16 20:58:46 -0600

    first commit

---
+++ Scripts/Selector.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using ProBuilder2.Common;
+using ProBuilder2.Math;
+using ProBuilder2.MeshOperations;
+
+public class Selector : MonoBehaviour {
+
+	public Camera camera;
+
+	pb_Object pb;
+
+	void Start () {
+		
+	}
+
+	private enum Face {None, Up, Down, Left, Right, Front, Back}
+
+	private Face getHitFace (RaycastHit hit){
+		Vector3 incomingVec = hit.normal - Vector3.up;
+		if (incomingVec == new Vector3 (0, -1, -1)) {return Face.Back;}
+		if (incomingVec == new Vector3 (0, -1, 1)) {return Face.Front;}
+		if (incomingVec == new Vector3 (0, 0, 0)) {return Face.Up;}
+		if (incomingVec == new Vector3 (1, 1, 1) || incomingVec == new Vector3 (0, -2, 0)) {return Face.Down;}
+		if (incomingVec == new Vector3 (-1, -1, 0)) {return Face.Left;}
+		if (incomingVec == new Vector3 (1, -1, 0)) {return Face.Right;}
+		return Face.None;
+	}
+
+	private Vector3 calcuatePosition (Vector3 vec, Face face){
+		float xOff = 0;
+		float yOff = 0;
+		float zOff = 0;
+
+		if (face == Face.Up) {
+			zOff++;
+		} else if (face == Face.Down) {
+			zOff++;
+			yOff--;
+		} else if (face == Face.Right) {
+			zOff++;
+		} else if (face == Face.Left) {
+			xOff--;
+			zOff++;
+		} else if (face == Face.Front) {
+			zOff++;
+		}
+		return new Vector3 (Mathf.Floor(vec.x + xOff), Mathf.Floor(vec.y + yOff), Mathf.Floor(vec.z + zOff));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray ray = new Ray (camera.transform.position, camera.transform.forward);
+
+		if (Physics.Raycast (ray, out hit)) {
+			GetComponent<MeshRenderer> ().enabled = true;
+			transform.position = calcuatePosition (hit.point, getHitFace(hit));
+		} else {
+			GetComponent<MeshRenderer> ().enabled = false;
+		}
+
+		if (GetComponent<MeshRenderer> ().enabled) {
+			if (Input.GetMouseButtonDown (0)) {
+				pb = pb_ShapeGenerator.CubeGenerator (Vector3.one);
+				pb.gameObject.transform.position = transform.position;
+			}
+		}
+	}
+}
